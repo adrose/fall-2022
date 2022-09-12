f04ac4ff77ce9de52cd0ba4c4a52ca0f8e6a6ab2
Adon Rosen <adon.rosen@gmail.com>
2022-09-12 16:10:04 -0500

    Add PS2

---
+++ ProblemSets/PS2-optimization-intro/ps2.jl
@@ -0,0 +1,100 @@
+using Optim, DataFrames, CSV, HTTP, GLM, FreqTables
+
+function all_ans()    
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    # question 1
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    #using Optim
+    f(x) = -x[1]^4-10x[1]^3-2x[1]^2-3x[1]-2
+    negf(x) = x[1]^4+10x[1]^3+2x[1]^2+3x[1]+2
+    startval = rand(16) 
+    result = optimize(negf, startval, LBFGS())
+
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    # question 2
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    #using DataFrames
+    #using CSV
+    #using HTTP
+    url = "https://raw.githubusercontent.com/OU-PhD-Econometrics/fall-2022/master/ProblemSets/PS1-julia-intro/nlsw88.csv"
+    df = CSV.read(HTTP.get(url).body, DataFrame)
+    X = [ones(size(df,1),1) df.age df.race.==1 df.collgrad.==1]
+    y = df.married.==1
+
+    function ols(beta, X, y)
+        ssr = (y.-X*beta)'*(y.-X*beta)
+        return ssr
+    end
+    beta_hat_ols = optimize(b -> ols(b, X, y), rand(size(X,2)), LBFGS(),Optim.Options(g_tol=1e-6, iterations=100_000,show_trace=true))
+    println(beta_hat_ols.minimizer)
+
+    #using GLM
+    bols = inv(X'*X)*X'*y
+    df.white = df.race.==1
+    bols_lm = lm(@formula(married ~ age + white + collgrad), df)
+
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    # question 3
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    function logit(alpha, X, d)
+        P = exp.(X*alpha)./(1 .+ exp.(X*alpha))
+        loglike = -sum( (d.==1).*log.(P) .+ (d.==0).*log.(1 .- P) ) 
+        return loglike
+    end
+    alpha_hat_optim = optimize(a -> logit(a, X, y), rand(size(X,2)), LBFGS(), Optim.Options(g_tol=1e-6, iterations=100_000, show_trace=true))
+    println(alpha_hat_optim.minimizer)
+
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    # question 4
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    alpha_hat_glm = glm(@formula(married ~ age + white + collgrad), df, Binomial(), LogitLink())
+    println(alpha_hat_glm)
+
+
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    # question 5
+    #:::::::::::::::::::::::::::::::::::::::::::::::::::
+    #using FreqTables
+    freqtable(df, :occupation) # note small number of obs in some occupations
+    df = dropmissing(df, :occupation)
+    df[df.occupation.==8 ,:occupation] .= 7
+    df[df.occupation.==9 ,:occupation] .= 7
+    df[df.occupation.==10,:occupation] .= 7
+    df[df.occupation.==11,:occupation] .= 7
+    df[df.occupation.==12,:occupation] .= 7
+    df[df.occupation.==13,:occupation] .= 7
+    freqtable(df, :occupation) # problem solved
+    X = [ones(size(df,1),1) df.age df.race.==1 df.collgrad.==1]
+    y = df.occupation
+
+    function mlogit(alpha, X, d)
+        K = size(X,2)
+        J = length(unique(d))
+        N = length(d)
+        bigD = zeros(N,J) 
+        for j=1:J
+            bigD[:,j] = d.==j
+        end
+        tmpAlpha = [reshape(alpha,K,J-1) zeros(K)]
+        num = zeros(N,J)
+        dem = zeros(N)
+        for j=1:J
+            num[:,j] = exp.(X*tmpAlpha[:,j])
+            dem .+= num[:,j]
+        end
+        P = num./repeat(dem,1,J) 
+        loglike = -sum( bigD.*log.(P) )
+        return loglike
+    end
+    #alpha_zero = zeros(6*size(X,2))
+    #alpha_rand = rand(6*size(X,2))
+    #alpha_true = [.1910213,-.0335262,.5963968,.4165052,-.1698368,-.0359784,1.30684,-.430997,.6894727,-.0104578,.5231634,-1.492475,-2.26748,-.0053001,1.391402,-.9849661,-1.398468,-.0142969,-.0176531,-1.495123,.2454891,-.0067267,-.5382892,-3.78975]
+    alpha_start = alpha_true.*rand(size(alpha_true))
+    println(size(alpha_true))
+    alpha_hat_optim = optimize(a -> mlogit(a, X, y), alpha_start, LBFGS(), Optim.Options(g_tol = 1e-5, iterations=100_000, show_trace=true, show_every=50))
+    alpha_hat_mle = alpha_hat_optim.minimizer
+    println(alpha_hat_mle)
+end
+
+
+all_ans()
